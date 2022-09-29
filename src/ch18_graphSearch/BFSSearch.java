4e2e81732854dff88ccb5a5d25670e4e0366fb89
BackJiwan <bn1357nb@gmail.com>
2022-09-29 21:33:44 +0900

    Practice

---
+++ src/ch18_graphSearch/BFSSearch.java
@@ -0,0 +1,42 @@
+package ch18_graphSearch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class BFSSearch {
+    static HashMap<String, ArrayList<String>> graph = new HashMap<String, ArrayList<String>>();
+
+    public ArrayList<String> bfsFunc(HashMap<String, ArrayList<String>> grpah, String startNode) {
+        ArrayList<String> visited = new ArrayList<String>();
+        ArrayList<String> needVisit = new ArrayList<String>();
+
+        needVisit.add(startNode);
+
+        while (needVisit.size() > 0) {
+            String node = needVisit.remove(0);
+
+            if (!visited.contains(node)) {
+                visited.add(node);
+                needVisit.addAll(graph.get(node));
+            }
+        }
+        return visited;
+    }
+
+    public static void main(String[] args) {
+        graph.put("A", new ArrayList<String>(Arrays.asList("B", "C")));
+        graph.put("B", new ArrayList<String>(Arrays.asList("A", "D")));
+        graph.put("C", new ArrayList<String>(Arrays.asList("A", "G", "H", "I")));
+        graph.put("D", new ArrayList<String>(Arrays.asList("B", "E", "F")));
+        graph.put("E", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("F", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("G", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("H", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("I", new ArrayList<String>(Arrays.asList("C", "J")));
+        graph.put("J", new ArrayList<String>(Arrays.asList("I")));
+
+        BFSSearch bObject = new BFSSearch();
+        System.out.println(bObject.bfsFunc(graph, "A"));
+    }
+}
